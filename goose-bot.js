a2000d950259a7fea8ca02ac27cf52c79a29ed63
6joeb <joe6ball@gmail.com>
2020-05-13 22:14:50 +0100

    initial bot setup

---
+++ goose-bot.js
@@ -0,0 +1,17 @@
+// Run dotenv
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === "test" ) {
+        msg.reply("honking");
+    }
+})
+
+client.login(process.env.DISCORD_TOKEN);
